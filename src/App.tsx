aabbe0c699611ebf68732b85d8c9d96932a30841
jch1223 <ico1828@gmail.com>
2022-01-28 20:26:53 +0900

    feat: 404 page 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "normalize.css";
+
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
