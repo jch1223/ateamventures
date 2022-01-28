7e7e48340cff43b347dac2103a6ea924bfcf0fa9
jch1223 <ico1828@gmail.com>
2022-01-28 20:26:53 +0900

    feat: 404 page 추가

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <article>
+      <h1>404</h1>
+      <span>Page Not Found</span>
+    </article>
+  );
+}
+
+export default NotFound;
