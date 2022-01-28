53467784be3bc0d64abfd6d73db2e8b499ce98f6
jch1223 <ico1828@gmail.com>
2022-01-28 20:53:41 +0900

    feat: styled component theme 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import "normalize.css";
-
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  palette: {
+    darkenBlue: "#1565c0",
+    blue: "#2196f3",
+    lightenBlue: "#bbdefb",
+    yellow: "#ffbe50",
+    gray: "#c2c2c2",
+    lightenGray: "#f5f5f5",
+  },
+};
