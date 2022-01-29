e1cb67a6a1334ceedb3aac7ccb57b322f4f27bb6
jch1223 <ico1828@gmail.com>
2022-01-29 22:44:30 +0900

    feat: 견적 요청 카드 레이아웃

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import EstimateCard from "../components/EstimateCard";
 
 import Layout from "../layouts/Layout";
 
@@ -10,11 +11,25 @@ function Home() {
         <h1>들어온 요청</h1>
         <span>파트너님에게 딱 맞는 요청서를 찾아보세요.</span>
       </TopContent>
+
+      <article>
+        <div>
+          <select name="가공방식"></select>
+          <select name="재료"></select>
+          <div>상담 중인 요청</div>
+        </div>
+
+        <div>
+          <EstimateCard />
+        </div>
+      </article>
     </Layout>
   );
 }
 
 const TopContent = styled.section`
+  margin-bottom: 30px;
+
   h1 {
     font-weight: 600;
     font-size: 20px;
